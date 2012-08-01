e9b8e821775adf862d1e16fef15bfca2fcc32b1f
Michael Cianfrocco <michael.a.cianfrocco@gmail.com>
2012-08-01 11:11:19 -0700

    Removed echo of refine.py command

--- lib/run_freehand.sh
+++ lib/run_freehand.sh
@@ -12,4 +12,3 @@ set p = $9
 
 mpirun -np 8 $p/refine.py $stack $model refine_eman2 --ou=$radius --rs=1 --xr=$sx --ts=$ts --delta=$ang --snr=$snr --center='0' --maxit=1 --ref_a=S --sym=c1 --cutoff=$cutoff --MPI  --full_output
 
-echo mpirun -np 8 $p/refine.py $stack $model refine_eman2 --ou=$radius --rs=1 --xr=$sx --ts=$ts --delta=$ang --snr=$snr --center='0' --maxit=1 --ref_a=S --sym=c1 --cutoff=$cutoff --MPI  --full_output
